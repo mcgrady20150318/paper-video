2422d5c11624b0e20973d9a2621e4aa168f6d7d4
mcgrady20150318 <mcgrady150318@gmail.com>
2023-11-09 22:17:48 +0800

    Update video.py

--- video.py
+++ video.py
@@ -58,7 +58,19 @@ def get_paper_info(id,max_results=1):
     abstract = result.summary
     return title,abstract.replace('\n',' ').replace('{','').replace('}','')
 
-def get_cover(title):
+def get_poster(text,id,idx):
+    header = Header(text=text,
+                    text_width=60,
+                    font=Font(path=path+'FangZhengKaiTi-GBK-1.ttf',size=20),
+                    align='center',
+                    color='#000100',
+                    )
+    content = Content(header=header)
+    img = Image(content,fullpath='./'+id+'/poster/'+str(idx) + '.png')
+    img.draw_on_image(path+'cover.jpg')
+    os.rename('./'+id+'/poster/'+str(idx) + '.png','./'+id+'/poster/'+str(idx) + '.jpg')
+
+def get_cover(title,id):
     header = Header(text=title,
                     text_width=50,
                     font=Font(path=path+'Handwritten-English-2.ttf',size=40),
@@ -77,8 +89,9 @@ def generate_readme(id):
     if not os.path.exists('./'+id):
         os.mkdir('./'+id)
         os.mkdir('./'+id+'/audio')
+        os.mkdir('./'+id+'/poster')
     title,abstract  = get_paper_info(id)
-    get_cover(title)
+    get_cover(title,id)
     prompt_template =  """现在你是Paperweekly论文电台分享主播Shirin，请根据论文标题"%s"和摘要"%s",生成一段电台播报风格的采访稿，严格按照一问一答的形式生成，角色分别是主播Shirin和AI研究人员Ian，生成内容如下：""" %(title,abstract)
     PROMPT = PromptTemplate(template=prompt_template, input_variables=[])
     chain = LLMChain(llm=llm, prompt=PROMPT)
@@ -110,29 +123,33 @@ def generate_radio(id):
         if _o[0] == a:
             _output = "".join(_o[1:])
             asyncio.run(gen_voice(_output,id,idx,girl))
+            get_poster(_output,id,idx)
         else:
             _output = "".join(_o[1:])
             asyncio.run(gen_voice(_output,id,idx,boy))
+            get_poster(_output,id,idx)
 
-    image_files = [id+'/cover.jpg'] * 10
-    audios = os.listdir('./'+id+'/audio/')
-    audios.sort(key=lambda x:int(x[:-4]))
-    audio_files = ['./'+id+'/audio/' + a for a in audios]
 
-    total_time = 0
-
-    for audio_file in audio_files:
-        total_time += get_time_count(audio_file)
-
-    audio_clip = concatenate_audioclips([AudioFileClip(c) for c in audio_files])
-    image_clip = ImageSequenceClip(image_files, fps=len(image_files)/total_time)
-    audio_clip.write_audiofile('./'+id+'/'+id+'.mp3')
+    image_folder = './'+id+'/poster'
+    audio_folder = './'+id+'/audio'
+    image_files = os.listdir(image_folder)
+    audio_files = os.listdir(audio_folder)
+    image_files.sort(key=lambda x:int(x[:-4]))
+    audio_files.sort(key=lambda x:int(x[:-4]))
+    audio_clips = concatenate_audioclips([AudioFileClip(os.path.join(audio_folder,c)) for c in audio_files])
+    image_clips = []
+    for idx, image in enumerate(image_files):
+        duration = get_time_count(os.path.join(audio_folder, audio_files[idx]))
+        _image = ImageClip(os.path.join(image_folder, image)).set_duration(duration)
+        image_clips.append(_image)
+    video = concatenate_videoclips(image_clips)
+    final_video = video.set_audio(audio_clips)
+    audio_clips.write_audiofile('./'+id+'/'+id+'.mp3')
     with open('./'+id+'/'+id+'.mp3', 'rb') as f:
         file_content = f.read()
     r.set('bilibili:'+id+':radio_'+id+".mp3",file_content)
     print('...generate radio done...')
-    video_clip = image_clip.set_audio(audio_clip)
-    video_clip.write_videofile('./'+id+'/'+id+'.mp4',codec='libx264')
+    final_video.write_videofile('./'+id+'/'+id+'.mp4',codec='libx264',fps=24)
     with open('./'+id+'/'+id+'.mp4', 'rb') as f:
         file_content = f.read()
     r.set('bilibili:'+id+':radio_'+id+".mp4",file_content)
@@ -154,8 +171,9 @@ def get_today_list(day=0):
     return arxivids
 
 if __name__ == '__main__':
-    ids = get_today_list()        
-    print(ids)
+    # ids = get_today_list()        
+    # print(ids)
+    ids = ['2311.04823']
     for id in ids:
         r.rpush('radio_paper',id)
         try:
